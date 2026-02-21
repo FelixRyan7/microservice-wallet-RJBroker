a50674a87f2e962fb770947476783c3e0406275b
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:09:42 +0100

    repository for portfolio

--- src/main/java/com/broker/wallet_service/repository/PortfolioRepository.java
+++ src/main/java/com/broker/wallet_service/repository/PortfolioRepository.java
@@ -1,2 +1,16 @@
-package com.broker.wallet_service.repository;public interface PortfolioRepository {
+package com.broker.wallet_service.repository;
+
+import com.broker.wallet_service.model.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+
+    Optional<Portfolio> findByUserIdAndAssetSymbol(Long userId, String assetSymbol);
+
+    List<Portfolio> findAllByUserId(Long userId);
 }
