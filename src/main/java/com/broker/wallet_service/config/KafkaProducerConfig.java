c52de947c148c65a90139da78fb9cda41461312c
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:56:25 +0100

    config for kafka producer

--- src/main/java/com/broker/wallet_service/config/KafkaProducerConfig.java
+++ src/main/java/com/broker/wallet_service/config/KafkaProducerConfig.java
@@ -1,2 +1,38 @@
-package com.broker.wallet_service.config;public class KafkaProducerConfig {
+package com.broker.wallet_service.config;
+
+import com.broker.wallet_service.events.WalletEvent;
+import com.broker.wallet_service.model.Wallet;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    private final String bootstrapServers = "localhost:9092";
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        // para que el JsonSerializer no agregue headers que compliquen la deserialización
+        configProps.put(JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
