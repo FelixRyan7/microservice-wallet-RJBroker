0c9a2ff580fe86ec599775e6b33b65596ad048b0
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:05:59 +0100

    model for asset price

--- src/main/java/com/broker/wallet_service/model/AssetPrice.java
+++ src/main/java/com/broker/wallet_service/model/AssetPrice.java
@@ -1,2 +1,39 @@
-package com.broker.wallet_service.model;public class AssetPrice {
+package com.broker.wallet_service.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "asset_prices")
+public class AssetPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String assetSymbol;
+
+    private LocalDate date; // fecha de mercado
+    private double closePrice;
+
+    public AssetPrice() {}
+
+    public AssetPrice(String assetSymbol, LocalDate date, double closePrice) {
+        this.assetSymbol = assetSymbol;
+        this.date = date;
+        this.closePrice = closePrice;
+    }
+
+    // getters setters
+    public Long getId() { return id; }
+    public String getAssetSymbol() { return assetSymbol; }
+    public LocalDate getDate() { return date; }
+    public double getClosePrice() { return closePrice; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setAssetSymbol(String assetSymbol) { this.assetSymbol = assetSymbol; }
+    public void setDate(LocalDate date) { this.date = date; }
+    public void setClosePrice(double closePrice) { this.closePrice = closePrice; }
 }
+
