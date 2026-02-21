5a5f85f7b0829f332cba49bc67e8fe457be92324
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:11:05 +0100

    ai service

--- src/main/java/com/broker/wallet_service/service/AiService.java
+++ src/main/java/com/broker/wallet_service/service/AiService.java
@@ -1,2 +1,55 @@
-package com.broker.wallet_service.service;public class AiService {
+package com.broker.wallet_service.service;
+
+import com.broker.wallet_service.DTOS.GeminiRequest;
+import com.broker.wallet_service.DTOS.GeminiResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpHeaders;
+import java.util.List;
+
+@Service
+public class AiService {
+
+    @Value("${ai.gemini.api-key}")
+    private String apiKey;
+
+    @Value("${ai.gemini.url}")
+    private String apiUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public String analyzeTerm(String prompt) {
+
+        String url = apiUrl;
+
+        GeminiRequest request = new GeminiRequest(
+                List.of(
+                        new GeminiRequest.Content(
+                                List.of(new GeminiRequest.Content.Part(prompt))
+                        )
+                )
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-goog-api-key", apiKey);
+
+        HttpEntity<GeminiRequest> entity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<GeminiResponse> response =
+                restTemplate.postForEntity(url, entity, GeminiResponse.class);
+
+        return response.getBody()
+                .candidates()
+                .get(0)
+                .content()
+                .parts()
+                .get(0)
+                .text();
+    }
 }
