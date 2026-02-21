c6057d6475e16ab71d07ae13fdf46bf043b3e093
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:03:44 +0100

    service for benchmark

--- src/main/java/com/broker/wallet_service/service/BenchMarkService.java
+++ src/main/java/com/broker/wallet_service/service/BenchMarkService.java
@@ -1,2 +1,49 @@
-package com.broker.wallet_service.service;public class BenchMarkService {
+package com.broker.wallet_service.service;
+
+import com.broker.wallet_service.DTOS.AssetPriceDto;
+import com.broker.wallet_service.DTOS.BenchmarkResult;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class BenchMarkService {
+
+    private final PoligonClient poligonClient;
+    private final FinnhubClient finnhubClient;
+
+    public BenchMarkService(PoligonClient poligonClient, FinnhubClient finnhubClient) {
+        this.poligonClient = poligonClient;
+        this.finnhubClient = finnhubClient;
+    }
+
+    public BenchmarkResult getBenchmark(String symbol, LocalDate startDate) {
+
+        System.out.println(startDate);
+        BigDecimal startPrice = poligonClient.getOpenPrice(symbol, startDate);
+        AssetPriceDto quote = finnhubClient.getQuote(symbol);
+        BigDecimal currentPrice = quote.price();
+
+        BigDecimal performance = BigDecimal.ZERO;
+
+        if (startPrice != null && startPrice.compareTo(BigDecimal.ZERO) > 0) {
+            performance = currentPrice
+                    .subtract(startPrice)
+                    .divide(startPrice, 6, BigDecimal.ROUND_UP)
+                    .multiply(BigDecimal.valueOf(100));
+        }
+
+        return new BenchmarkResult(symbol, startPrice, currentPrice, performance);
+    }
+
+    public List<BenchmarkResult> getBenchmarks(LocalDate startDate) {
+        return List.of(
+                getBenchmark("SPY", startDate),
+                getBenchmark("QQQ", startDate),
+                getBenchmark("VGK", startDate),
+                getBenchmark("URTH", startDate)
+        );
+    }
 }
