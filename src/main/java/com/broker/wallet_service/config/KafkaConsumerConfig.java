31c7e4ede5a340192c52de285fd54dedb54faf8e
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:56:03 +0100

    config for kafka consumer

--- src/main/java/com/broker/wallet_service/config/KafkaConsumerConfig.java
+++ src/main/java/com/broker/wallet_service/config/KafkaConsumerConfig.java
@@ -1,2 +1,45 @@
-package com.broker.wallet_service.config;public class KafkaConsumerConfig {
+package com.broker.wallet_service.config;
+
+import com.broker.wallet_service.events.UserCreatedEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    private final String bootstrapServers = "localhost:9092";
+    private final String groupId = "wallet-service-group1";
+
+    @Bean
+    public ConsumerFactory<String, UserCreatedEvent> consumerFactory() {
+        JacksonJsonDeserializer<UserCreatedEvent> deserializer = new JacksonJsonDeserializer<>(UserCreatedEvent.class, false);
+        deserializer.addTrustedPackages("com.broker.wallet_service.events"); // paquetes confiables
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // para procesar desde el primer mensaje si el consumer es nuevo
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UserCreatedEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, UserCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
+
