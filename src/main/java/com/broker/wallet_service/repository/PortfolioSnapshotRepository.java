63c5eaa06d0f32cfe72f00265c0321219f5fcfe2
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:07:52 +0100

    repository for portfolio snapshot

--- src/main/java/com/broker/wallet_service/repository/PortfolioSnapshotRepository.java
+++ src/main/java/com/broker/wallet_service/repository/PortfolioSnapshotRepository.java
@@ -1,2 +1,17 @@
-package com.broker.wallet_service.repository;public interface PortfolioSnapshotRepository {
+package com.broker.wallet_service.repository;
+
+import com.broker.wallet_service.model.PortfolioSnapshot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PortfolioSnapshotRepository extends JpaRepository<PortfolioSnapshot, Long> {
+
+    // buscar todos los snapshots de un usuario ordenados por snapshotDate asc
+    List<PortfolioSnapshot> findByUserIdOrderBySnapshotDateAsc(Long userId);
+    Optional<PortfolioSnapshot> findByUserIdAndSnapshotDate(Long userId, LocalDate snapshotDate);
 }
