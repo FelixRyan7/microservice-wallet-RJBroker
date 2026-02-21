48c043388aa2af0f0f5b760caab280ae72924571
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:00:55 +0100

    service for portfolio snapshot model

--- src/main/java/com/broker/wallet_service/service/PortfolioSnapshotService.java
+++ src/main/java/com/broker/wallet_service/service/PortfolioSnapshotService.java
@@ -1,2 +1,24 @@
-package com.broker.wallet_service.service;public class PortfolioSnapshotService {
+package com.broker.wallet_service.service;
+
+import com.broker.wallet_service.model.PortfolioSnapshot;
+import com.broker.wallet_service.repository.PortfolioSnapshotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PortfolioSnapshotService {
+
+    @Autowired
+    private PortfolioSnapshotRepository snapshotRepository;
+
+    public List<PortfolioSnapshot> getUserSnapshots(Long userId) {
+        return snapshotRepository.findByUserIdOrderBySnapshotDateAsc(userId);
+    }
+    public Optional<PortfolioSnapshot> getSnapshotByDate(Long userId, LocalDate snapshotDate) {
+        return snapshotRepository.findByUserIdAndSnapshotDate(userId, snapshotDate);
+    }
 }
