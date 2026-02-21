6016acc16c113f8686a7f2a937bccd85436436a4
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 17:09:04 +0100

    repository for wallet

--- src/main/java/com/broker/wallet_service/repository/WalletRepository.java
+++ src/main/java/com/broker/wallet_service/repository/WalletRepository.java
@@ -1,2 +1,18 @@
-package com.broker.wallet_service.repository;public interface WalletRepository {
+package com.broker.wallet_service.repository;
+
+import com.broker.wallet_service.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    boolean existsByUserId(Long userId);
+
+    Optional<Wallet> findByUserId(Long userId);
+
+    @Query("select distinct w.userId from Wallet w")
+    List<Long> findDistinctUserIds();
+
 }
